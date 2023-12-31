5e89530a7460b65d0fb12307e09179b1f16ed931
Chinonso <chinonsoalexander2@gmail.com>
2023-12-31 13:43:58 +0100

    updated otp verification 5

--- Payuee/auth/auth-page-in.js
+++ Payuee/auth/auth-page-in.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
 function get_auth_status() {
     if (localStorage.getItem('auth') === 'true') {
         // let's redirect to a non-authenticated page cause the user is not authenticated
-        window.location.href = '../index.html';
+        window.location.href = 'index.html';
         // let's clear any local storage item
         localStorage.removeItem('auth');
     }
