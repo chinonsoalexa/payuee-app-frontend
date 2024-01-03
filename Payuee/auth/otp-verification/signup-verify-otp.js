6e1e41ef329b891b70e470565ba34c8ee8b1d502
Chinonso <chinonsoalexander2@gmail.com>
2024-01-03 21:18:33 +0100

    updated verify email for signup

--- Payuee/auth/otp-verification/signup-verify-otp.js
+++ Payuee/auth/otp-verification/signup-verify-otp.js
@@ -37,8 +37,6 @@ document.addEventListener('DOMContentLoaded', async function () {
                 const errorData = await response.json();
                 // Check the error message
                 // Handle fetch-related errors
-                console.log(errorData);
-                console.log('error message: ', errorData.error);
                 if (errorData.error === 'failed to get OTP') {
                     // Perform actions specific to this error
                     showError('otpError', 'an error occurred verifying email .');
@@ -48,7 +46,8 @@ document.addEventListener('DOMContentLoaded', async function () {
                 } else if  (errorData.error === 'OTP  Expired') {
                     // redirect user to verify email ID
                     showError('otpError', 'This email verification link has expired please try sending another one.');
-                    // window.location.href = '/verify';
+                    // if error is otp expired let's show the user button to resend an otp verification link
+                    document.getElementById('resendOtp').style.display='block';
                 } else {
                     showError('passwordError', 'An error occurred. Please try again.');
                 }
@@ -66,6 +65,52 @@ document.addEventListener('DOMContentLoaded', async function () {
         hideLoadingIcon();
 });
 
+document.getElementById("submitButtonContainer").addEventListener('click', async function () {
+    // Get the current URL
+    const currentUrl = new URL(window.location.href);
+
+    // Extract parameters using URLSearchParams
+    const params = new URLSearchParams(currentUrl.search);
+
+    // Get individual parameter values
+    const userID = params.get("user");
+
+    const resendOTP = {
+        Email: userID,
+    };
+
+    const apiUrl = "https://payuee.onrender.com/resend-otp";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        },
+        credentials: 'include', // set credentials to include cookies
+        body: JSON.stringify(resendOTP),
+    };
+    
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+
+        if (!response.ok) {
+            // throw new Error(`HTTP error! Status: ${response.status}`);
+            data = await response.json();
+            if (data.error == 'Failed to get previous email OTP') {
+                showError('otpError', "Email not found, please re-enter your email address.");
+            } else {
+                showError('otpError', 'An error occurred. Please try again.');
+            }
+            return;
+        } 
+        const data = await response.json();
+    } finally {
+        // do nothing cause error has been handled
+        reactivateButtonStyles();
+    }
+    reactivateButtonStyles();
+})
+
 // Function to show loading icon
 function showLoadingIcon() {
     loadingIcon.style.display = 'inline';
@@ -79,4 +124,19 @@ function hideLoadingIcon() {
 function showError(id, message) {
     var errorElement = document.getElementById(id);
     errorElement.textContent = message;
+}
+
+// Add this function to reactivate the button styles
+function reactivateButtonStyles() {
+    var resendButton = document.getElementById('submitPassword');
+    resendButton.classList.remove('deactivated');
+    
+    clearError('otpError');
+}
+
+// Add this function to remove onclick and on hover styles
+function deactivateInputStyles() {
+    var currentInput = document.getElementById('submitPassword');
+    // Disable the input field
+    currentInput.disabled = true;
 }
