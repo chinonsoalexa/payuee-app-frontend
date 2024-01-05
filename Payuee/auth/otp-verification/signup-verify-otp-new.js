27b236bfd30d691422a1b07cdbaa6bcb5dee6bb9
Chinonso <chinonsoalexander2@gmail.com>
2024-01-05 20:24:28 +0100

    update sign up page2

--- Payuee/auth/otp-verification/signup-verify-otp-new.js
+++ Payuee/auth/otp-verification/signup-verify-otp-new.js
@@ -63,7 +63,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             hideLoadingIcon();
             showError('otpError', 'Email address verified...');
             localStorage.setItem('auth', 'true');
-            window.location.href = 'https://payuee.vercel.app/index-in.html'
+            window.location.href = '../index-in.html'
         } finally{
            // do nothing cause error has been handled
             hideLoadingIcon();
