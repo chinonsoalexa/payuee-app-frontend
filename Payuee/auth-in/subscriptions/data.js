081abf0a77f82f0d315297b611fd83e83ba0c152
Chinonso <chinonsoalexander2@gmail.com>
2024-01-13 13:53:45 +0100

    updated the airtime recharge page

--- Payuee/auth-in/subscriptions/data.js
+++ Payuee/auth-in/subscriptions/data.js
@@ -159,13 +159,9 @@ async function getSelectedPlan(dataValue) {
     // niceSelectCurrentSpan.textContent = operatorSelect.options[operatorSelect.selectedIndex].text;
 
     // Check if the selected value is not the default option
-    // Check if the selected value is not the default option
     if (dataValue !== '1') {
-        var plansSelect = document.getElementById('planSelectId');
-        
-        // Check if the list is empty
-        if (!plansSelect.querySelector('.nice-select .list .option')) {
-            plansSelect.innerHTML = '<option value="plans">Select a Plan</option>';
+        // var plansSelect = document.getElementById('planSelectId');
+        // plansSelect.innerHTML = '<option value="plans">Select a Plan</option>';
 
         // Perform a task based on the selected value
         switch (dataValue) {
