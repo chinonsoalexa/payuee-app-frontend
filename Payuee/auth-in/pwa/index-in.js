20317307356e0ce382b68c269149727595a06674
Chinonso <chinonsoalexander2@gmail.com>
2024-01-18 03:42:34 +0100

    paystack payment integration

--- Payuee/auth-in/pwa/index-in.js
+++ Payuee/auth-in/pwa/index-in.js
@@ -59,7 +59,7 @@ let deferredPrompt;
 
     // Register the service worker
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/Payuee/auth-in/pwa/service-worker.js')
+      navigator.serviceWorker.register('/Payuee/auth-in/pwa/service-worker.js', { scope: '/' })
         .then(registration => {
           console.log('Service Worker registered with scope:', registration.scope);
         })
