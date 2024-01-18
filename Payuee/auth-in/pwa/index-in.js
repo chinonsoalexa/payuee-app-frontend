fcde12b2919b03bb366cf48efaf8888687157575
Chinonso <chinonsoalexander2@gmail.com>
2024-01-18 01:26:46 +0100

    paystack payment integration

--- Payuee/auth-in/pwa/index-in.js
+++ Payuee/auth-in/pwa/index-in.js
@@ -10,14 +10,17 @@ const isAppInstalled = () => {
   return window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone;
 };
 
-// Function to show the install button
-const showInstallButton = () => {
-  // Display a button or UI element to prompt the user to install the app
-  const installButton = document.getElementById('install-button');
-  installButton.style.display = 'block';
+// Event listener for beforeinstallprompt
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  event.preventDefault();
 
-  installButton.addEventListener('click', () => {
-    // Trigger the deferredPrompt to show the install prompt
+  // Stash the event so it can be triggered later
+  deferredPrompt = event;
+
+  // Check if the user is on a mobile device and the PWA is not installed
+  if (isMobileDevice() && !isAppInstalled()) {
+    // Show the browser's default install prompt
     deferredPrompt.prompt();
 
     // Wait for the user to respond to the prompt
@@ -30,25 +33,6 @@ const showInstallButton = () => {
 
       // Reset the deferredPrompt to null
       deferredPrompt = null;
-
-      // Hide the install button
-      installButton.style.display = 'none';
     });
-  });
-};
-
-// Check if the user is on a mobile device and the PWA is not installed after 5 seconds
-setTimeout(() => {
-  if (isMobileDevice() && !isAppInstalled()) {
-    showInstallButton();
   }
-}, 5000);
-
-// Event listener for beforeinstallprompt
-window.addEventListener('beforeinstallprompt', (event) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  event.preventDefault();
-
-  // Stash the event so it can be triggered later
-  deferredPrompt = event;
 });
