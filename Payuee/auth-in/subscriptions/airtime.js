6190f1d3fe1945bef9bf0162edc98d42c9ca6ee7
Chinonso <chinonsoalexander2@gmail.com>
2024-01-19 12:25:42 +0100

    updated the database update function for verify transaction

--- Payuee/auth-in/subscriptions/airtime.js
+++ Payuee/auth-in/subscriptions/airtime.js
@@ -20,7 +20,7 @@ document.getElementById('back-to-airtime').addEventListener('click', function(ev
 
 document.getElementById('continue-buy-airtime').addEventListener('click', async function(event) {
     event.preventDefault();
-    // var amount = parseInt(amountInput.value, 10);
+    var amount = parseInt(amountInput.value, 10);
     var updatedTransactionCharge = amount;
     // Using toString()
     var finalAmountString1 = updatedTransactionCharge.toString();
