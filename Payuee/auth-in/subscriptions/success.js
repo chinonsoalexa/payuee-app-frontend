261fe7952453f7dd29b04eb155cc7082dde19344
Chinonso <chinonsoalexander2@gmail.com>
2024-01-19 18:52:56 +0100

    updated the database update function for verify transaction

--- Payuee/auth-in/subscriptions/success.js
+++ Payuee/auth-in/subscriptions/success.js
@@ -19,9 +19,9 @@ window.onload = async function() {
 
             if (errorData.error === 'failed to get user from request') {
                 // need to do a data of just null event 
-                transactionDetails = '';
+                displayErrorMessage();
             } else {
-                transactionDetails = '';
+                displayErrorMessage();
             }
 
             return;
@@ -60,4 +60,30 @@ function getSuccessMessage(transactionDetails) {
     transaction_status.textContent = transactionDetails.success.transaction_status;
     users_name.textContent = transactionDetails.success.user_name;
     transaction_amount.textContent = '₦' + transactionDetails.success.fees;
+}
+
+function displayErrorMessage() {
+
+    var available_balance = document.getElementById('available_balance');
+    var transaction_id = document.getElementById('transaction_id');
+    var transaction_date = document.getElementById('transaction_date');
+    var transaction_method = document.getElementById('transaction_method');
+    var transaction_status = document.getElementById('transaction_status');
+    var users_name = document.getElementById('users_name');
+    // var recharged_number = document.getElementById('recharged_number');
+    // var service_name = document.getElementById('service_name');
+    var transaction_amount = document.getElementById('transaction_amount');
+
+    // Access balance directly
+    available_balance.textContent = 'NA';
+
+    // Access properties within the success object using dot notation
+    transaction_id.textContent = 'NA';
+    
+    transaction_date.textContent = 'NA'; // Adjust the format as needed
+
+    transaction_method.textContent = 'NA';
+    transaction_status.textContent = 'NA';
+    users_name.textContent = 'NA';
+    transaction_amount.textContent = '₦' + 'NA';
 }
