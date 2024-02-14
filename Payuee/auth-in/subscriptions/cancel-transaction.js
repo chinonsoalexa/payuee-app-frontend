97d7dd19ead10e534f94403ad6ecc20f0ef6027a
Chinonso <chinonsoalexander2@gmail.com>
2024-02-14 15:59:30 +0100

    updated the airtime json request to send price along when making payments with wallet

---
+++ Payuee/auth-in/subscriptions/cancel-transaction.js
@@ -0,0 +1,26 @@
+window.onload = async function () {
+    const apiUrl = "https://payuee.onrender.com/paystack/get-latest-transaction";
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: 'include', // set credentials to include cookies
+    };
+
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+
+        const responseData = await response.json();
+        displayErrorMessage(responseData.balance)
+    } finally {
+
+    }
+}
+
+function displayErrorMessage(balance) {
+    var available_balance = document.getElementById('available_balance');
+
+    available_balance.textContent = balance;
+}
