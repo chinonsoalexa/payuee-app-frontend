6b8896dcfa277db1bbaa87fe21283a193a70b060
Chinonso <chinonsoalexander2@gmail.com>
2024-03-20 13:08:22 +0100

    working on the transfer to payuee wallet frontend

---
+++ Payuee/auth-in/profile/personal-information.js
@@ -0,0 +1,85 @@
+// make request to the server for users profile details on load of the page
+var referral_link
+
+var SHOW_EDIT = false;
+
+document.getElementById('edit_button').addEventListener('click', function (event) {
+    event.preventDefault()
+    if (!SHOW_EDIT) {
+        SHOW_EDIT = true;
+        showEdit();
+    } else if (SHOW_EDIT) {
+        SHOW_EDIT = false;
+        hideEdit();
+        // let's make a request to the server to update the user's profile details
+    }
+})
+
+function showEdit() {
+    document.getElementById('toggle-first-name-main').classList.add('disabled');
+    document.getElementById('toggle-first-name-main').disabled = true;
+    document.getElementById('edit_button_one').classList.add('disabled');
+    document.getElementById('edit_button_one').disabled = true;
+    document.getElementById('toggle-last-name-main').classList.add('disabled');
+    document.getElementById('toggle-last-name-main').disabled = true;
+    document.getElementById('toggle-address-main').classList.add('disabled');
+    document.getElementById('toggle-address-main').disabled = true;
+
+    document.getElementById('toggle-first-name').classList.remove('disabled');
+    document.getElementById('toggle-first-name').disabled = false;
+    document.getElementById('save_button_one').classList.remove('disabled');
+    document.getElementById('save_button_one').disabled = false;
+    document.getElementById('toggle-last-name').classList.remove('disabled');
+    document.getElementById('toggle-last-name').disabled = false;
+    document.getElementById('toggle-address').classList.remove('disabled');
+    document.getElementById('toggle-address').disabled = false;
+}
+
+function hideEdit() {
+    document.getElementById('toggle-first-name-main').classList.remove('disabled');
+    document.getElementById('toggle-first-name-main').disabled = false;
+    document.getElementById('edit_button_one').classList.remove('disabled');
+    document.getElementById('edit_button_one').disabled = false;
+    document.getElementById('toggle-last-name-main').classList.remove('disabled');
+    document.getElementById('toggle-last-name-main').disabled = false;
+    document.getElementById('toggle-address-main').classList.remove('disabled');
+    document.getElementById('toggle-address-main').disabled = false;
+
+    document.getElementById('toggle-first-name').classList.add('disabled');
+    document.getElementById('toggle-first-name').disabled = true;
+    document.getElementById('save_button_one').classList.add('disabled');
+    document.getElementById('save_button_one').disabled = true;
+    document.getElementById('toggle-last-name').classList.add('disabled');
+    document.getElementById('toggle-last-name').disabled = true;
+    document.getElementById('toggle-address').classList.add('disabled');
+    document.getElementById('toggle-address').disabled = true;
+}
+
+document.getElementById('referral_link').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // Select and copy the content
+    navigator.clipboard.writeText('https://payuee.vercel.app/Payuee/page/signin-new.html' + referral_link)
+    .then(() => {
+        // Success
+        referralLinkCopier();
+        console.log('success');
+    })
+    .catch((err) => {
+        // Handle error
+        // copyBtn.textContent = 'error...';
+    });
+
+})
+
+function referralLinkCopier() {
+    const installPopup = document.getElementById('referral-popup');
+    const cancelButton = document.getElementById('cancel-btn');
+
+      installPopup.style.display = 'block';
+
+    // Cancel button click event
+    cancelButton.addEventListener('click', () => {
+      installPopup.style.display = 'none';
+    });
+}
