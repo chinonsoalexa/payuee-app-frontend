e9ce0c76ea4816be1f75519d7cefd40ec870d333
Chinonso <chinonsoalexander2@gmail.com>
2024-03-22 20:18:13 +0100

    working on the profile information

--- Payuee/auth-in/profile/transaction.js
+++ Payuee/auth-in/profile/transaction.js
@@ -98,7 +98,7 @@ document.addEventListener('DOMContentLoaded', async function () {
             currentPageAnchor.textContent = NextPageOnLoad;
         }
 
-        if (TwoAfterPageOnLoad < TotalPageOnLoad) {
+        if (TwoAfterPageOnLoad <= TotalPageOnLoad) {
             // let's update the pagination with the next page
             var currentPageElement = document.getElementById("twoAfterPage");
             var currentPageAnchor = currentPageElement.querySelector("a");
