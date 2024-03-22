ed95acf86714c66fabd3a47f74238622cbfa49ba
Chinonso <chinonsoalexander2@gmail.com>
2024-03-22 22:39:42 +0100

    working on the profile information

--- Payuee/auth-in/profile/transaction.js
+++ Payuee/auth-in/profile/transaction.js
@@ -215,7 +215,6 @@ function renderTransactionHistory(historyData) {
             // Retrieve the ID of the clicked row
             const rowId = event.target.closest('tr').id;
             // Use the ID as needed
-            // console.log('Clicked row ID:', rowId);
             window.location.href = 'success-trans-id.html?id=' + rowId;
         });
     });
