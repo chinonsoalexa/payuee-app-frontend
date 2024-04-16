37556e32e4e097758e0e839d3d1faa1372f08bfc
Chinonso <chinonsoalexander2@gmail.com>
2024-04-16 19:33:48 +0100

    updated the create user

--- auth-in/subscriptions/email-subscription.js
+++ auth-in/subscriptions/email-subscription.js
@@ -51,40 +51,39 @@ document.addEventListener("DOMContentLoaded", function() {
             showPopup("An error occurred while trying to add email");
         }
     });
+    
+    // Function to create and display the popup box
+    function showPopup(message) {
+        // Create elements for the popup box
+        var popup = document.createElement("div");
+        popup.id = "balance-popup";
+        popup.classList.add("popup");
+    
+        var popupContent = document.createElement("div");
+        popupContent.classList.add("popup-content");
+    
+        var messageParagraph = document.createElement("p");
+        messageParagraph.id = "emailMessage";
+        messageParagraph.textContent = message;
+    
+        var closeButton = document.createElement("button");
+        closeButton.classList.add("cancel-button");
+        closeButton.textContent = "Close";
+    
+        // Add event listener to close button
+        closeButton.addEventListener("click", function() {
+        // Hide the popup when close button is clicked
+        popup.style.display = "none";
+        });
+    
+        // Append elements to the popup content
+        popupContent.appendChild(messageParagraph);
+        popupContent.appendChild(closeButton);
+    
+        // Append popup content to the popup
+        popup.appendChild(popupContent);
+    
+        // Append popup to the body
+        document.body.appendChild(popup);
+    }
 });
-
-
-// Function to create and display the popup box
-function showPopup(message) {
-    // Create elements for the popup box
-    var popup = document.createElement("div");
-    popup.id = "balance-popup";
-    popup.classList.add("popup");
-  
-    var popupContent = document.createElement("div");
-    popupContent.classList.add("popup-content");
-  
-    var messageParagraph = document.createElement("p");
-    messageParagraph.id = "emailMessage";
-    messageParagraph.textContent = message;
-  
-    var closeButton = document.createElement("button");
-    closeButton.classList.add("cancel-button");
-    closeButton.textContent = "Close";
-  
-    // Add event listener to close button
-    closeButton.addEventListener("click", function() {
-      // Hide the popup when close button is clicked
-      popup.style.display = "none";
-    });
-  
-    // Append elements to the popup content
-    popupContent.appendChild(messageParagraph);
-    popupContent.appendChild(closeButton);
-  
-    // Append popup content to the popup
-    popup.appendChild(popupContent);
-  
-    // Append popup to the body
-    document.body.appendChild(popup);
-}
