6e4fdf547a102bb8349c608823246c2cd5981a2d
Chinonso <chinonsoalexander2@gmail.com>
2024-04-16 17:25:41 +0100

    updated the create user

---
+++ auth-in/subscriptions/email-subscription.js
@@ -0,0 +1,4 @@
+document.getElementById('email_rocket_subscription').addEventListener('click', function(event) {
+    // Prevent the default behavior (in this case, the redirect)
+    event.preventDefault();
+})
