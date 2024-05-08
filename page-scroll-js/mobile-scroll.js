bad832e4031f7bf188794829f56fc871aac9ac2a
Chinonso <chinonsoalexander2@gmail.com>
2024-05-08 15:40:26 +0100

    initial commit 900

--- page-scroll-js/mobile-scroll.js
+++ page-scroll-js/mobile-scroll.js
@@ -4,10 +4,37 @@ document.addEventListener('DOMContentLoaded', function(event) {
     const currentUrl = new URL(window.location.href);
 
     // Extract origin and pathname
-    const urlWithoutQuery = currentUrl.origin + currentUrl.pathname;
+    const pathName =  currentUrl.pathname;
 
-    console.log(urlWithoutQuery, " this is the path name: ", currentUrl.pathname); // Output: 'https://payuee.com/data'
-
+    // Check if the clicked link has a specific class or ID
+    if (event.target.classList.contains('airtime-id') || event.target.id === 'airtime-id' || pathName === '/airtime') {
+        event.preventDefault();
+        scrollToSection('airtime-section1');
+    } else if (event.target.classList.contains('card-id') || event.target.id === 'card-pin-id') {
+        event.preventDefault();
+        scrollToSection('card-pin-section1');
+    } else if (event.target.classList.contains('data-id') || event.target.id === 'data-id') {
+        event.preventDefault();
+        scrollToSection('data-section1');
+    } else if (event.target.classList.contains('education-id') || event.target.id === 'education-id') {
+        event.preventDefault();
+        scrollToSection('education-section');
+    } else if (event.target.classList.contains('tv-id') || event.target.id === 'tv-id') {
+        event.preventDefault();
+        scrollToSection('tv-section1');
+    } else if (event.target.classList.contains('electricity-id') || event.target.id === 'electricity-id') {
+        event.preventDefault();
+        scrollToSection('electricity-section1');
+    } else if (event.target.classList.contains('wallet-id') || event.target.id === 'wallet-id') {
+        event.preventDefault();
+        scrollToSection('wallet-section1');
+    } else if (event.target.classList.contains('send-funds-id') || event.target.id === 'send-funds-id') {
+        event.preventDefault();
+        scrollToSection('send-funds-section1');
+    } else if (event.target.classList.contains('withdraw-funds-id') || event.target.id === 'withdraw-funds-id') {
+        event.preventDefault();
+        scrollToSection('withdraw-funds-section1');
+    }
 });
 
 // Handle scrolling when a link is clicked
