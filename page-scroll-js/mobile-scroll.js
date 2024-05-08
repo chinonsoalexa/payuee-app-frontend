e9be42b9b391b8a23d318b626e3852a5db7576d1
Chinonso <chinonsoalexander2@gmail.com>
2024-05-08 16:05:54 +0100

    initial commit 534

--- page-scroll-js/mobile-scroll.js
+++ page-scroll-js/mobile-scroll.js
@@ -2,7 +2,6 @@
 document.addEventListener('DOMContentLoaded', function(event) {
     if ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
         // Code to run only on mobile phones or ipads
-
         // Get the current URL
         const currentUrl = new URL(window.location.href);
         const pathName = currentUrl.pathname;
@@ -26,36 +25,71 @@ document.addEventListener('DOMContentLoaded', function(event) {
             scrollToSection('send-funds-section1');
         }
     }
-
 });
 
 // Handle scrolling when a link is clicked
 document.addEventListener('click', function (event) {
-    // Check if the clicked link has a specific class or ID
-    if (event.target.classList.contains('airtime-id') || event.target.id === 'airtime-id') {
-        event.preventDefault();
-        scrollToSection('airtime-section1');
-    } else if (event.target.classList.contains('card-id') || event.target.id === 'card-pin-id') {
-        event.preventDefault();
-        scrollToSection('card-pin-section1');
-    } else if (event.target.classList.contains('data-id') || event.target.id === 'data-id') {
-        event.preventDefault();
-        scrollToSection('data-section1');
-    } else if (event.target.classList.contains('education-id') || event.target.id === 'education-id') {
-        event.preventDefault();
-        scrollToSection('education-section');
-    } else if (event.target.classList.contains('tv-id') || event.target.id === 'tv-id') {
-        event.preventDefault();
-        scrollToSection('tv-section1');
-    } else if (event.target.classList.contains('electricity-id') || event.target.id === 'electricity-id') {
-        event.preventDefault();
-        scrollToSection('electricity-section1');
-    } else if (event.target.classList.contains('wallet-id') || event.target.id === 'wallet-id') {
-        event.preventDefault();
-        scrollToSection('wallet-section1');
-    } else if (event.target.classList.contains('send-funds-id') || event.target.id === 'send-funds-id') {
-        event.preventDefault();
-        scrollToSection('send-funds-section1');
+    if ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        // Code to run only on mobile phones or ipads
+        // Check if the clicked link has a specific class or ID
+        if (event.target.classList.contains('airtime-id') || event.target.id === 'airtime-id') {
+            event.preventDefault();
+            scrollToSection('airtime-section1');
+        } else if (event.target.classList.contains('card-id') || event.target.id === 'card-pin-id') {
+            event.preventDefault();
+            scrollToSection('card-pin-section1');
+        } else if (event.target.classList.contains('data-id') || event.target.id === 'data-id') {
+            event.preventDefault();
+            scrollToSection('data-section1');
+        } else if (event.target.classList.contains('education-id') || event.target.id === 'education-id') {
+            event.preventDefault();
+            scrollToSection('education-section');
+        } else if (event.target.classList.contains('tv-id') || event.target.id === 'tv-id') {
+            event.preventDefault();
+            scrollToSection('tv-section1');
+        } else if (event.target.classList.contains('electricity-id') || event.target.id === 'electricity-id') {
+            event.preventDefault();
+            scrollToSection('electricity-section1');
+        } else if (event.target.classList.contains('wallet-id') || event.target.id === 'wallet-id') {
+            event.preventDefault();
+            scrollToSection('wallet-section1');
+        } else if (event.target.classList.contains('send-funds-id') || event.target.id === 'send-funds-id') {
+            event.preventDefault();
+            scrollToSection('send-funds-section1');
+        }
+    } else {
+        // Check if the clicked link has a specific class or ID
+        if (event.target.classList.contains('airtime-id') || event.target.id === 'airtime-id') {
+            event.preventDefault();
+            scrollToSection('airtime-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('card-id') || event.target.id === 'card-pin-id') {
+            event.preventDefault();
+            scrollToSection('card-pin-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('data-id') || event.target.id === 'data-id') {
+            event.preventDefault();
+            scrollToSection('data-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('education-id') || event.target.id === 'education-id') {
+            event.preventDefault();
+            scrollToSection('education-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('tv-id') || event.target.id === 'tv-id') {
+            event.preventDefault();
+            scrollToSection('tv-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('electricity-id') || event.target.id === 'electricity-id') {
+            event.preventDefault();
+            scrollToSection('electricity-section');
+            scrollToSection('invoice-section');
+        } else if (event.target.classList.contains('wallet-id') || event.target.id === 'wallet-id') {
+            event.preventDefault();
+            scrollToSection('wallet-section');
+        } else if (event.target.classList.contains('send-funds-id') || event.target.id === 'send-funds-id') {
+            event.preventDefault();
+            scrollToSection('send-funds-section');
+        }
     }
 });
 
