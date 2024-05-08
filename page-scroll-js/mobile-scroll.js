6c5695eb49a69f6f069e1f9c3e9c2496fd06b960
Chinonso <chinonsoalexander2@gmail.com>
2024-05-08 15:33:45 +0100

    initial commit 900

--- page-scroll-js/mobile-scroll.js
+++ page-scroll-js/mobile-scroll.js
@@ -1,33 +1,14 @@
-// Handle scrolling when a link is clicked
-// document.addEventListener('DOMContentLoaded', function (event) {
-//     // Check if the clicked link has a specific class or ID
-//     if (event.target.classList.contains('airtime-id') || event.target.id === 'airtime-id') {
-//         scrollToSection('airtime-section');
-//         scrollToSection('airtime-invoice-section');
-//     } else if (event.target.classList.contains('card-id') || event.target.id === 'card-pin-id') {
-//         scrollToSection('card-pin-section');
-//         scrollToSection('card-pin-invoice-section');
-//     } else if (event.target.classList.contains('data-id') || event.target.id === 'data-id') {
-//         scrollToSection('data-section');
-//         scrollToSection('data-invoice-section');
-//     } else if (event.target.classList.contains('education-id') || event.target.id === 'education-id') {
-//         scrollToSection('education-section');
-//         scrollToSection('education-invoice-section');
-//     } else if (event.target.classList.contains('tv-id') || event.target.id === 'tv-id') {
-//         scrollToSection('tv-section');
-//         scrollToSection('tv-invoice-section');
-//     } else if (event.target.classList.contains('electricity-id') || event.target.id === 'electricity-id') {
-//         scrollToSection('electricity-section');
-//         scrollToSection('electricity-invoice-section');
-//     } else if (event.target.classList.contains('wallet-id') || event.target.id === 'wallet-id') {
-//         scrollToSection('wallet-section');
-//     } else if (event.target.classList.contains('send-funds-id') || event.target.id === 'send-funds-id') {
-//         scrollToSection('send-funds-section');
-//     } else if (event.target.classList.contains('withdraw-funds-id') || event.target.id === 'withdraw-funds-id') {
-//         scrollToSection('withdraw-funds-section');
-//     }
-// });
+// Focus on the first input field when the page loads
+document.addEventListener('DOMContentLoaded', function(event) {
+    // Get the current URL
+    const currentUrl = new URL(window.location.href);
 
+    // Extract origin and pathname
+    const urlWithoutQuery = currentUrl.origin + currentUrl.pathname;
+
+    console.log(urlWithoutQuery, " this is the path name: ", currentUrl.pathname); // Output: 'https://payuee.com/data'
+
+});
 
 // Handle scrolling when a link is clicked
 document.addEventListener('click', function (event) {
