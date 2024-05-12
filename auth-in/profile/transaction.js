f0edbfcd4fc4409002f884b59b1544a90946a902
Chinonso <chinonsoalexander2@gmail.com>
2024-05-12 13:43:49 +0100

    initial commit 5343

--- auth-in/profile/transaction.js
+++ auth-in/profile/transaction.js
@@ -543,7 +543,7 @@ document.getElementById("currentPage").addEventListener("click", async function(
 
 document.getElementById("nextPage").addEventListener("click", async function(event){
     event.preventDefault(); 
-
+    renderTransactionHistoryLoading();
     const apiUrl = "https://api.payuee.com/transactions/" + NextPageOnLoad;
     
         const requestOptions = {
@@ -579,7 +579,8 @@ document.getElementById("nextPage").addEventListener("click", async function(eve
             }
     
             const responseData = await response.json();
-            window.location.href = 'transaction.html?page=' + NextPageOnLoad;
+            getNextPageByClick(responseData, NextPageOnLoad);
+            // window.location.href = 'transaction.html?page=' + NextPageOnLoad;
     } finally {
     
     }
@@ -677,7 +678,6 @@ document.getElementById("twoAfterPage").addEventListener("click", async function
     event.preventDefault(); 
 
     const apiUrl = "https://api.payuee.com/transactions/" + TwoAfterPageOnLoad;
-    
         const requestOptions = {
             method: "GET",
             headers: {
@@ -789,3 +789,107 @@ document.getElementById('refreshHistory').addEventListener('click', function(eve
     event.preventDefault(); // Prevent the default behavior of the anchor tag
     location.reload(); // Call the function to reload the page
 });
+
+function getNextPageByClick(responseData, currentPageNumber) {
+    // render the transaction history
+    renderTransactionHistory(responseData.success);
+
+    NextPageOnLoad = responseData.pagination.NextPage;
+    PreviousPageOnLoad = responseData.pagination.PreviousPage;
+    CurrentPageOnLoad = responseData.pagination.CurrentPage;
+    TotalPageOnLoad = responseData.pagination.TotalPages;
+    TwoBeforePageOnLoad = responseData.pagination.TwoBefore;
+    TwoAfterPageOnLoad = responseData.pagination.TwoAfter;
+    ThreeAfterPageOnLoad = responseData.pagination.ThreeAfter;
+    AllRecordsOnPageLoad = responseData.pagination.AllRecords;
+    if (AllRecordsOnPageLoad > 6) {
+        // let's disable the next page navigation button
+        document.getElementById('paginationList').classList.remove('disabled');
+        document.getElementById('paginationList').disabled = false;
+    }
+
+    if (CurrentPageOnLoad <= 1) {
+        deactivatePreviousButton();
+        deactivateBeforeButton();
+    } else if (CurrentPageOnLoad >= responseData.pagination.TotalPages) {
+        deactivateNextButton();
+    }
+
+    if (CurrentPageOnLoad < 4) {
+        // let's disable the next page navigation button
+        document.getElementById('constantBeforePage').classList.add('disabled');
+        document.getElementById('constantBeforePage').disabled = true;
+    }
+
+    if (CurrentPageOnLoad < 5) {
+        // let's disable the next page navigation button
+        document.getElementById('dotBeforePage').classList.add('disabled');
+        document.getElementById('dotBeforePage').disabled = true;
+    }
+
+    if (CurrentPageOnLoad > 2) {
+        // let's update the pagination with the next page
+        var currentPageElement = document.getElementById("twoBeforePage");
+        var currentPageAnchor = currentPageElement.querySelector("a");
+        currentPageAnchor.textContent = TwoBeforePageOnLoad;
+    } else {
+        // let's disable the next page navigation button
+        document.getElementById('twoBeforePage').classList.add('disabled');
+        document.getElementById('twoBeforePage').disabled = true;
+    }
+
+    // let's update the pagination with the next page
+    var currentPageElement = document.getElementById("beforePage");
+    var currentPageAnchor = currentPageElement.querySelector("a");
+    currentPageAnchor.textContent = PreviousPageOnLoad;
+
+    // let's update the pagination with the current page
+    var currentPageElement = document.getElementById("currentPage");
+    var currentPageAnchor = currentPageElement.querySelector("a");
+    currentPageAnchor.textContent = CurrentPageOnLoad;
+    deactivateCurrentButton();
+
+    if (CurrentPageOnLoad >= TotalPageOnLoad) {
+        // let's disable the next page navigation button
+        document.getElementById('afterPage').classList.add('disabled');
+        document.getElementById('afterPage').disabled = true;
+    } else {
+        // let's update the pagination with the next page
+        var currentPageElement = document.getElementById("afterPage");
+        var currentPageAnchor = currentPageElement.querySelector("a");
+        currentPageAnchor.textContent = NextPageOnLoad;
+    }
+
+    if (TwoAfterPageOnLoad < TotalPageOnLoad) {
+        // let's update the pagination with the next page
+        var currentPageElement = document.getElementById("twoAfterPage");
+        var currentPageAnchor = currentPageElement.querySelector("a");
+        currentPageAnchor.textContent = TwoAfterPageOnLoad;
+    } else {
+        // let's disable the next page navigation button
+        document.getElementById('twoAfterPage').classList.add('disabled');
+        document.getElementById('twoAfterPage').disabled = true;
+    }
+
+    if (TwoAfterPageOnLoad > TotalPageOnLoad) {
+        // let's disable the next page navigation button
+        document.getElementById('constantAfterPage').classList.add('disabled');
+        document.getElementById('constantAfterPage').disabled = true;
+    } else {
+        // let's update the pagination with the next page
+        var currentPageElement = document.getElementById("constantAfterPage");
+        var currentPageAnchor = currentPageElement.querySelector("a");
+        currentPageAnchor.textContent = TotalPageOnLoad;
+    }
+
+    if (ThreeAfterPageOnLoad > TotalPageOnLoad) {
+        // let's disable the next page navigation button
+        document.getElementById('dotAfterPage').classList.add('disabled');
+        document.getElementById('dotAfterPage').disabled = true;
+    }
+
+    // Update the URL parameter without reloading the page
+    const newUrl = new URL(window.location.href);
+    newUrl.searchParams.set('page', currentPageNumber);
+    window.history.pushState({path: newUrl.href}, '', newUrl.href);
+}
