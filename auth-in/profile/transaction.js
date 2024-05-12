b249a284718c9c5a5f635a44cc9a6359a2a670d8
Chinonso <chinonsoalexander2@gmail.com>
2024-05-12 14:30:51 +0100

    initial commit 5343

--- auth-in/profile/transaction.js
+++ auth-in/profile/transaction.js
@@ -830,7 +830,7 @@ function getNextPageByClick(responseData, currentPageNumber) {
         // let's disable the next page navigation button
         document.getElementById('paginationList').classList.remove('disabled');
         document.getElementById('paginationList').disabled = false;
-    } else if (currentPageNumber < TotalPageOnLoad) {
+    } else if (CurrentPageOnLoad < AllRecordsOnPageLoad) {
         document.getElementById('paginationList').classList.add('disabled');
         document.getElementById('paginationList').disabled = true;
     }
