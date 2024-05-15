494a145f8e0b80d75c6b6226989775c66272268f
Chinonso <chinonsoalexander2@gmail.com>
2024-05-15 20:51:46 +0100

    working on session time out

--- auth/auth-page-in.js
+++ auth/auth-page-in.js
@@ -1,4 +1,3 @@
-
 // Listen for the DOMContentLoaded event
 document.addEventListener('DOMContentLoaded', function () {
     var analyticsData = sessionStorage.getItem('analyticsData');
@@ -18,6 +17,66 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // this would be for unauthenticated pages
 function get_auth_status() {
+
+	// Key does not exist in localStorage
+	sessionStorage.setItem('analyticsData', 'true');
+
+        // send a post request with the email and password
+        const otp = {
+            Email: email,
+            Password: password,
+            };
+
+            const apiUrl = "https://api.payuee.com/auth-status";
+
+            const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include', // set credentials to include cookies
+            };
+            
+        try {
+            const response = fetch(apiUrl, requestOptions);
+            
+
+            if (!response.ok) {
+                const errorData = response.json();
+
+                if  (errorData.error === 'No Authentication cookie found' || errorData.error === "Unauthorized attempt! JWT's not valid!") {
+                    // let's log user out the users session has expired
+					Swal.fire({
+						title: "The Internet?",
+						text: "That thing is still around?",
+						icon: "question",
+						confirmButtonColor: "#556ee6"
+					}).then((result) => {
+						// Check if the user clicked the confirmation button
+						if (result.isConfirmed) {
+							// Task to perform after the user clicks OK
+							logUserOutIfTokenIsExpired();
+							// console.log("User clicked OK");
+							// Call your function or execute your code here
+						} else {
+							// Task to perform if the user clicks outside the dialog or cancels
+							logUserOutIfTokenIsExpired();
+							// console.log("User clicked outside the dialog or cancelled");
+							// Call your function or execute your code here
+						}
+					});
+                } else {
+					logUserOutIfTokenIsExpired();
+					// showError('passwordError', 'An error occurred. Please try again.');
+                }
+
+                return;
+            }
+            localStorage.setItem('auth', 'true');
+        } finally{
+            
+        }
+    
     if (localStorage.getItem('auth') === 'true') {
         // let's redirect to a authenticated page cause the user is not authenticated
         window.location.href = '../index-in.html';
@@ -78,3 +137,26 @@ function get_auth_status() {
 			return fetchPromise;
 		};
 	})();
+
+	function logUserOutIfTokenIsExpired() {
+		
+		// also send a request to the logout api endpoint
+		const apiUrl = "https://api.payuee.com/log-out";
+	
+		const requestOptions = {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: 'include', // set credentials to include cookies
+		};
+		
+	try {
+		const response = fetch(apiUrl, requestOptions);
+	
+			// const data = response.json();
+			localStorage.removeItem('auth')
+			window.location.href = 'page/signin-new.html'
+		} finally{
+		}
+	}
