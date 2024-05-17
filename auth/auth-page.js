55ffd0aa7a2fc6b8b4c217f7e9542e12a223ac16
Chinonso <chinonsoalexander2@gmail.com>
2024-05-17 09:30:56 +0100

    working on session time out

--- auth/auth-page.js
+++ auth/auth-page.js
@@ -215,6 +215,50 @@ function check_auth_status() {
     }
 }
 
+async function check_auth_status() {
+    const apiUrl = "https://api.payuee.com/auth-status";
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: 'include', // set credentials to include cookies
+    };
+
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+
+        if (!response.ok) {
+            const errorData = await response.json();
+
+            if  (errorData.error === 'No Authentication cookie found' || errorData.error === "Unauthorized attempt! JWT's not valid!") {
+                Swal.fire({
+                    title: "Session Expired",
+                    text: "Please Try To Login Again Your Session Has Expired!!!",
+                    icon: "info",
+                    confirmButtonColor: "#556ee6"
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        logout();
+                    } else {
+                        logout();
+                    }
+                });
+            } else {
+                logout();
+            }
+            return;
+        }
+        localStorage.setItem('auth', 'true');
+    } finally {
+        if (localStorage.getItem('auth') !== 'true') {
+            window.location.href = 'page/signin-new.html';
+        }
+    }
+}
+
+
 // function logUserOutIfTokenIsExpired() {
 //     // also send a request to the logout api endpoint
 //     const apiUrl = "https://api.payuee.com/log-out";
