91e8afd42857ba4f11e0a2067c4b6377d97e9d0a
Chinonso <chinonsoalexander2@gmail.com>
2024-07-10 01:29:38 +0100

    updated payuee whatsapp channel icon

--- page-scroll-js/social.js
+++ page-scroll-js/social.js
@@ -3,28 +3,59 @@ document.addEventListener('DOMContentLoaded', function() {
     var isDragging = false;
     var startX, startY, initialX, initialY;
 
-    element.addEventListener('mousedown', function(e) {
+    function onMouseDown(e) {
         isDragging = true;
         startX = e.clientX;
         startY = e.clientY;
         initialX = element.offsetLeft;
         initialY = element.offsetTop;
         element.style.cursor = 'grabbing';
-    });
+    }
 
-    document.addEventListener('mousemove', function(e) {
+    function onMouseMove(e) {
         if (isDragging) {
             var dx = e.clientX - startX;
             var dy = e.clientY - startY;
             element.style.left = initialX + dx + 'px';
             element.style.top = initialY + dy + 'px';
         }
-    });
+    }
 
-    document.addEventListener('mouseup', function() {
+    function onMouseUp() {
         isDragging = false;
         element.style.cursor = 'pointer';
-    });
+    }
+
+    function onTouchStart(e) {
+        isDragging = true;
+        var touch = e.touches[0];
+        startX = touch.clientX;
+        startY = touch.clientY;
+        initialX = element.offsetLeft;
+        initialY = element.offsetTop;
+    }
+
+    function onTouchMove(e) {
+        if (isDragging) {
+            var touch = e.touches[0];
+            var dx = touch.clientX - startX;
+            var dy = touch.clientY - startY;
+            element.style.left = initialX + dx + 'px';
+            element.style.top = initialY + dy + 'px';
+        }
+    }
+
+    function onTouchEnd() {
+        isDragging = false;
+    }
+
+    element.addEventListener('mousedown', onMouseDown);
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
+    element.addEventListener('touchstart', onTouchStart);
+    document.addEventListener('touchmove', onTouchMove);
+    document.addEventListener('touchend', onTouchEnd);
 
     // Prevent default dragging behavior
     element.ondragstart = function() {
