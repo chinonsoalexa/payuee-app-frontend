f02d8a91fb581c919359b5cd0d659c3b99b531e2
Chinonso <chinonsoalexander2@gmail.com>
2024-09-28 05:43:36 +0100

    updating product details

--- e-shop/Demo3/js-shop/product-details/product_car.js
+++ e-shop/Demo3/js-shop/product-details/product_car.js
@@ -275,9 +275,9 @@ function renderProductDetails(product) {
             </div>
           </div>
           <div class="product-single__additional-info">
-            <a href="/e-shop/Demo3/#" data-bs-toggle="modal" data-bs-target="#deliveryModal">Composition and Care</a>
-            <a href="/e-shop/Demo3/#" data-bs-toggle="modal" data-bs-target="#deliveryModal">In-Store Availability</a>
-            <a href="/e-shop/Demo3/#" data-bs-toggle="modal" data-bs-target="#deliveryModal">Delivery and Return</a>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#deliveryModal">Composition and Care</a>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#deliveryModal">In-Store Availability</a>
+            <a href="#" data-bs-toggle="modal" data-bs-target="#deliveryModal">Delivery and Return</a>
           </div>
         </div>
         <div class="col-lg-5">
@@ -285,23 +285,23 @@ function renderProductDetails(product) {
             <div class="mb-md-1 pb-md-3"></div>
             <div class="d-flex justify-content-between mb-4 pb-md-2">
               <div class="breadcrumb mb-0 d-none d-md-block flex-grow-1">
-                <a href="/e-shop/Demo3/#" class="menu-link menu-link_us-s text-uppercase fw-medium">Home</a>
+                <a href="#" class="menu-link menu-link_us-s text-uppercase fw-medium">Home</a>
                 <span class="breadcrumb-separator menu-link fw-medium ps-1 pe-1">/</span>
-                <a href="/e-shop/Demo3/#" class="menu-link menu-link_us-s text-uppercase fw-medium">The Shop</a>
+                <a href="#" class="menu-link menu-link_us-s text-uppercase fw-medium">The Shop</a>
               </div><!-- /.breadcrumb -->  
               <div class="product-single__prev-next d-flex align-items-center justify-content-between justify-content-md-end flex-grow-1">
-                <a id="previousDetail" href="/e-shop/Demo3/product11_v6.html" class="text-uppercase fw-medium"><svg class="mb-1px" width="10" height="10" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_prev_md" /></svg><span class="menu-link menu-link_us-s">Prev</span></a>
-                <a id="nextDetail" href="/e-shop/Demo3/product13_v8.html" class="text-uppercase fw-medium"><span class="menu-link menu-link_us-s">Next</span><svg class="mb-1px" width="10" height="10" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_next_md" /></svg></a>
+                <a id="previousDetail" href="/e-shop/Demo3/product11_v6.html" class="text-uppercase fw-medium"><svg class="mb-1px" width="10" height="10" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><use href="#icon_prev_md" /></svg><span class="menu-link menu-link_us-s">Prev</span></a>
+                <a id="nextDetail" href="/e-shop/Demo3/product13_v8.html" class="text-uppercase fw-medium"><span class="menu-link menu-link_us-s">Next</span><svg class="mb-1px" width="10" height="10" viewBox="0 0 25 25" xmlns="http://www.w3.org/2000/svg"><use href="#icon_next_md" /></svg></a>
               </div><!-- /.shop-acs -->
             </div>
             <h1 class="product-single__name">Lightweight Puffer Jacket With a Hood</h1>
             <div class="product-single__rating">
               <div class="reviews-group d-flex">
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
               </div>
               <span class="reviews-note text-lowercase text-secondary ms-1">8k+ reviews</span>
             </div>
@@ -327,7 +327,7 @@ function renderProductDetails(product) {
                     <input type="radio" name="size" id="swatch-5">
                     <label class="swatch js-swatch" for="swatch-5" aria-label="Extra Large" data-bs-toggle="tooltip" data-bs-placement="top" title="Extra Large">XL</label>
                   </div>
-                  <a href="/e-shop/Demo3/#" class="sizeguide-link" data-bs-toggle="modal" data-bs-target="#sizeGuide">Size Guide</a>
+                  <a href="#" class="sizeguide-link" data-bs-toggle="modal" data-bs-target="#sizeGuide">Size Guide</a>
                 </div>
                 <div class="product-swatch color-swatches">
                   <label>Color</label>
@@ -351,10 +351,10 @@ function renderProductDetails(product) {
               </div>
             </form>
             <div class="product-single__addtolinks">
-              <a href="/e-shop/Demo3/#" class="menu-link menu-link_us-s add-to-wishlist"><svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_retweet" /></svg><span>Re-post Product</span></a>
+              <a href="#" class="menu-link menu-link_us-s add-to-wishlist"><svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_retweet" /></svg><span>Re-post Product</span></a>
               <share-button class="share-button">
                 <button class="menu-link menu-link_us-s to-share border-0 bg-transparent d-flex align-items-center">
-                  <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_sharing" /></svg>
+                  <svg width="16" height="19" viewBox="0 0 16 19" fill="none" xmlns="http://www.w3.org/2000/svg"><use href="#icon_sharing" /></svg>
                   <span>Share</span>
                 </button>
                 <details id="Details-share-template__main" class="m-1 xl:m-1.5" hidden="">
@@ -391,9 +391,9 @@ function renderProductDetails(product) {
               </div>
             </div>
             <div class="product-single__details">
-              <a href="/e-shop/Demo3/#" class="js-open-aside" data-aside="productDescription">Description</a>
-              <a href="/e-shop/Demo3/#" class="js-open-aside" data-aside="productAdditionalInformation">Additional Information</a>
-              <a href="/e-shop/Demo3/#" class="js-open-aside" data-aside="productReviews">Reviews (3)</a>
+              <a href="#" class="js-open-aside" data-aside="productDescription">Description</a>
+              <a href="#" class="js-open-aside" data-aside="productAdditionalInformation">Additional Information</a>
+              <a href="#" class="js-open-aside" data-aside="productReviews">Reviews (3)</a>
             </div>
           </div>
         </div>
@@ -776,11 +776,11 @@ function renderProductDescription(product) {
             <div class="customer-name">
               <h6>Janice Miller</h6>
               <div class="reviews-group d-flex">
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
               </div>
             </div>
             <div class="review-date">April 06, 2023</div>
@@ -797,11 +797,11 @@ function renderProductDescription(product) {
             <div class="customer-name">
               <h6>Benjam Porter</h6>
               <div class="reviews-group d-flex">
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+                <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
               </div>
             </div>
             <div class="review-date">April 06, 2023</div>
@@ -897,7 +897,7 @@ function renderRecommendedProduct() {
 
     rowElement.innerHTML = `
     <div class="pc__img-wrapper">
-        <a href="/e-shop/Demo3/#">
+        <a href="#">
           <img loading="lazy" src="/e-shop/Demo3/${product.Image1}" width="330" height="400" alt="${product.title}" class="pc__img">
           <img loading="lazy" src="/e-shop/Demo3/${product.Image2}" width="330" height="400" alt="${product.title}" class="pc__img pc__img-second">
         </a>
@@ -913,11 +913,11 @@ function renderRecommendedProduct() {
       </div>
       <div class="product-card__review d-flex align-items-center">
         <div class="reviews-group d-flex">
-          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
-          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>
+          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
+          <svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>
         </div>
         <span class="reviews-note text-lowercase text-secondary ms-1">${formatNumber(344)} reviews</span>
       </div>
@@ -1305,10 +1305,10 @@ function removeFromCart(productId) {
 function createStarRating(rating) {
   let stars = '';
   for (let i = 0; i < rating; i++) {
-    stars += `<svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star" /></svg>`;
+    stars += `<svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star" /></svg>`;
   }
   for (let i = rating; i < 5; i++) {
-    stars += `<svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="/e-shop/Demo3/#icon_star_outline" /></svg>`;
+    stars += `<svg class="review-star" viewBox="0 0 9 9" xmlns="http://www.w3.org/2000/svg"><use href="#icon_star_outline" /></svg>`;
   }
   return stars;
 }
