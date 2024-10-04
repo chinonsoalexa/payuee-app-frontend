d58b62ed32571b7f5a37fe80d4203aad12ee5391
Chinonso <chinonsoalexander2@gmail.com>
2024-10-04 05:33:57 +0100

    added payuee e-shop

---
+++ e-shop/Demo3/js-shop/auth/side-login/auth.js
@@ -0,0 +1,25 @@
+document.getElementById('myAccount').addEventListener('click', function(event) {
+    // window.location.href = 'your-target-page.html'; // Replace with your desired URL
+    event.preventDefault(); // Prevent the default link behavior
+    console.log('fgfgfg');
+    // window.location.href = 'your-target-page.html'; // Replace with your desired URL
+    const asideElement = document.getElementById("customerForms");
+    console.log('fgfgfg2');
+
+  // Add a class to open the aside
+  asideElement.classList.add('open');
+});
+  
+
+document.getElementById('myAccount2').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    // window.location.href = 'your-target-page.html'; // Replace with your desired URL
+    console.log('fgfgfg');
+    // window.location.href = 'your-target-page.html'; // Replace with your desired URL
+    const asideElement = document.getElementById("customerForms");
+    console.log('fgfgfg2');
+
+  // Add a class to open the aside
+  asideElement.classList.add('open');
+});
+  
