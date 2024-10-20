ac5f3a1b7b9496dc832580969e34bf87f515aaf1
Chinonso <chinonsoalexander2@gmail.com>
2024-10-20 18:04:50 +0100

    updating letluxe comment

--- auth-in/store/store-transactions.js
+++ auth-in/store/store-transactions.js
@@ -288,15 +288,15 @@ function renderProducts(product) {
           cancellationStatus.innerText = 'You are eligible to cancel this order.';
           cancellationStatus.style.color = 'green';
     
-          cancelButton.classList.add('disabled');
-          reportIssueButton.classList.remove('disabled');
-          transactionPinToCancelTrn.classList.add('disabled');
+          cancelButton.classList.remove('disabled');
+          reportIssueButton.classList.add('disabled');
+          transactionPinToCancelTrn.classList.remove('disabled');
         } else {
           cancellationStatus.innerText = 'You are no longer eligible to cancel this order.';
           cancellationStatus.style.color = 'red';
     
           cancelButton.classList.remove('disabled');
-          reportIssueButton.classList.add('disabled');
+        //   reportIssueButton.classList.add('disabled');
           transactionPinToCancelTrn.classList.remove('disabled');
         }
         // renderOrderedProducts(product);
