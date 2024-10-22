0423da2d725a88fdba60ac4d9c121aa569e9b725
Chinonso <chinonsoalexander2@gmail.com>
2024-10-22 14:04:13 +0100

    working on the cancel order on payuee e-Shop

--- auth-in/store/store-transactions.js
+++ auth-in/store/store-transactions.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', async function () {
     // Call the loading function to render the skeleton loaders
     loading();
     // showToast("hi i'm a skeleton loader");
-    showModal("transactionSuccessModal");
+    // showModal("transactionSuccessModal");
     // Get the current URL
     const currentUrl = new URL(window.location.href);
     // Assuming you have a reference to the table body element
