de4f1d3f8e9d42ace0342fbd5a893629377e9ebe
Chinonso <chinonsoalexander2@gmail.com>
2024-10-26 23:46:55 +0100

    working on the cancel order on payuee e-Shop

--- e-shop/Demo3/js-shop/product-details/product_cate.js
+++ e-shop/Demo3/js-shop/product-details/product_cate.js
@@ -59,7 +59,7 @@ function getCurrentUrl(title, description) {
 
 async function getProduct(productID) {
     renderLoadingDetails();
-  const apiUrl = "https://api.payuee.com/product/" + productID + "/outfit";
+  const apiUrl = "https://api.payuee.com/product/" + productID + "/outfits";
 
   const requestOptions = {
       method: "GET",
