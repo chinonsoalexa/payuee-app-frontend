5186c75f664ac95b09f0d134fccf862cd1ecdfc4
Chinonso <chinonsoalexander2@gmail.com>
2024-10-27 20:41:24 +0100

    working on the cancel order on payuee e-Shop

--- e-shop/Demo3/js-shop/product-details/product_ca.js
+++ e-shop/Demo3/js-shop/product-details/product_ca.js
@@ -1258,7 +1258,7 @@ function updateCartDrawer() {
           // Generate the HTML for the cart item
           cartItem.innerHTML = `
               <div class="position-relative">
-                <img loading="lazy" class="cart-drawer-item__img" src="${"https://payuee.com/image/"+cartProduct.Image1}" alt="">
+                <img loading="lazy" class="cart-drawer-item__img" src="${"https://payuee.com/image/"+cartProduct.product_image[0].url}" alt="">
               </div>
               <div class="cart-drawer-item__info flex-grow-1">
                 <h6 class="cart-drawer-item__title fw-normal">${cartProduct.title}</h6>
