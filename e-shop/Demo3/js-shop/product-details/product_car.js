101258015ed200ea94427266433bd3f818b9e43c
Chinonso <chinonsoalexander2@gmail.com>
2024-10-28 01:52:13 +0100

    working on the cancel order on payuee e-Shop

--- e-shop/Demo3/js-shop/product-details/product_car.js
+++ e-shop/Demo3/js-shop/product-details/product_car.js
@@ -318,7 +318,7 @@ function renderProductDetails(product, related) {
               </div>
               <div class="product-single__addtocart">
                 <div class="qty-control position-relative">
-                  <input type="number" name="quantity" value="1" min="1" class="qty-control__number text-center">
+                  <input type="number" name="quantity" value="1" min="1" max="${product.stock_remaining}" class="qty-control__number text-center">
                   <div class="qty-control__reduce">-</div>
                   <div class="qty-control__increase">+</div>
                 </div><!-- .qty-control -->
@@ -1213,7 +1213,7 @@ function updateCartDrawer() {
                 <p class="cart-drawer-item__option text-secondary">Netweight: ${cartProduct.net_weight}</p>
                 <div class="d-flex align-items-center justify-content-between mt-1">
                   <div class="qty-control position-relative">
-                    <input type="number" name="quantity" value="${cartProduct.quantity}" min="1" class="qty-control__number border-0 text-center">
+                    <input type="number" name="quantity" value="${cartProduct.quantity}" min="1" max="${cartProduct.stock_remaining}" class="qty-control__number border-0 text-center">
                     <div class="qty-control__reduce text-start" data-id="${cartProduct.ID}">-</div>
                     <div class="qty-control__increase text-end" data-id="${cartProduct.ID}">+</div>
                   </div>
