59574609b9d828aee4a06e512572a08854393a6c
Chinonso <chinonsoalexander2@gmail.com>
2024-10-30 14:03:10 +0100

    working on the pricing on payuee e-Shop

--- e-shop/Demo3/js-shop/pricing/pricing.js
+++ e-shop/Demo3/js-shop/pricing/pricing.js
@@ -1,10 +1,10 @@
 const trialBadge = document.querySelector('.trial-badge');
 
 trialBadge.addEventListener('click', () => {
-  // Add tilt-shake class
+  // Add the 'tilt-shake' class to trigger the animation on click
   trialBadge.classList.add('tilt-shake');
   
-  // Remove tilt-shake class after animation duration (0.5s here)
+  // Remove the class after the animation duration (0.5s)
   setTimeout(() => {
     trialBadge.classList.remove('tilt-shake');
   }, 500);
