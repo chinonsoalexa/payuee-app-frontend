ba115a027122a3a451bbf891e143f1749efd0c12
Chinonso <chinonsoalexander2@gmail.com>
2024-10-30 14:28:40 +0100

    working on the pricing on payuee e-Shop

--- e-shop/Demo3/js-shop/pricing/pricing.js
+++ e-shop/Demo3/js-shop/pricing/pricing.js
@@ -1,11 +1,11 @@
-const trialBadge = document.querySelector('.trial-badge');
+// const trialBadge = document.querySelector('.trial-badge');
 
-trialBadge.addEventListener('click', () => {
-  // Add the 'tilt-shake' class to trigger the animation on click
-  trialBadge.classList.add('tilt-shake');
+// trialBadge.addEventListener('click', () => {
+//   // Add the 'tilt-shake' class to trigger the animation on click
+//   trialBadge.classList.add('tilt-shake');
   
-  // Remove the class after the animation duration (0.5s)
-  setTimeout(() => {
-    trialBadge.classList.remove('tilt-shake');
-  }, 700);
-});
+//   // Remove the class after the animation duration (0.5s)
+//   setTimeout(() => {
+//     trialBadge.classList.remove('tilt-shake');
+//   }, 700);
+// });
