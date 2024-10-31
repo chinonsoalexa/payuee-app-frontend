8d57671edf83c4a4f170f6707888209e32b70517
Chinonso <chinonsoalexander2@gmail.com>
2024-10-31 07:05:25 +0100

    working on the pricing on payuee e-Shop

--- e-shop/Demo3/js-shop/pricing/pricing.js
+++ e-shop/Demo3/js-shop/pricing/pricing.js
@@ -84,10 +84,11 @@ function processPayment() {
                     showModal('transactionSuccessModal');
                     document.getElementById('transactionCodeInput').value = "";
                 } else {
-                    showToastMessageE(result.error);
+                    hideModal('checkoutModal');
+                    showModal('insufficientBalanceModal');
                 }
             } catch (error) {
-                showToastMessageE(error.error);
+                // showToastMessageE(error.error);
             }
             document.getElementById('transactionCodeInput').value = "";
         }
@@ -198,102 +199,30 @@ async function getUsersBalance() {
 }
 
 async function placeOrder() {
-    let OrderCost = 0.0;
-
-    const checkbox = document.getElementById('ship_different_address');
+    const checkbox = document.getElementById('autoRenewCheckbox');
     const isChecked = checkbox.checked;
 
-    // Construct the order history body
-    const orderHistoryBody = {
-        order_cost: parseFloat(OrderCost.toFixed(2)),
-        order_sub_total_cost: parseFloat(orderSubTotalCost.toFixed(2)),
-        shipping_cost: parseFloat(shippingCost.toFixed(2)),
-        order_discount: parseFloat(orderDiscount.toFixed(2)),
-        quantity: 0,
-        customer_email: customerEmail,
-        order_note: orderNotes,
-        customer_fname: customerFName,
-        customer_user_sname: customerSName,
-        customer_company_name: customerCompanyName,
-        customer_state: customerState,
-        customer_city: customerCity,
-        customer_street_address_1: customerStreetAddress1,
-        customer_street_address_2: customerStreetAddress2,
-        customer_zip_code: customerZipCode,
-        customer_province: customerProvince,
-        customer_phone_number: customerPhoneNumber,
-        save_shipping_address: isChecked
-    };
-
-    // Get cart from local storage
-    let cart = JSON.parse(localStorage.getItem('cart')) || [];
-
-    // Iterate through each product in the cart
-    cart.forEach((product) => {
-        if (product.selling_price < product.initial_cost) {
-            product.order_cost = parseFloat(product.selling_price.toFixed(2));
-            OrderCost += product.selling_price;
-        } else {
-            product.order_cost = parseFloat(product.initial_cost.toFixed(2));
-            OrderCost += product.initial_cost;
-        }
-    });
-
-    // Update order_cost with the final value after the loop
-    orderHistoryBody.order_cost = parseFloat(OrderCost.toFixed(2));
-
-    // Fields you want to keep
-    const desiredFields = [
-        'ID',
-        'category',
-        'title',
-        'description',
-        'user_id',
-        'eshop_user_id',
-        'product_url_id',
-        'currency',
-        'featured',
-        'order_cost',
-        'net_weight',
-        'quantity',
-        'product_image',
-        'initial_cost',
-        'selling_price',
-        'estimated_delivery'
-    ];
-
-    // Function to clean cart items
-    const cleanCartItems = (items) => {
-        return items.map(item => {
-            return desiredFields.reduce((acc, field) => {
-                acc[field] = item[field];
-                return acc;
-            }, {});
-        });
-    };
-
-    // Clean the cart item
-    const cleanedCartItem = cleanCartItems(cart);
-
-    // Create new orders from the cart
-    const newOrders = createNewOrders(cleanedCartItem, orderHistoryBody);
-
-    const checkoutButton = document.getElementById('placeOrderButton');
+    const checkoutButton = document.getElementById('paymentButton');
     
     checkoutButton.disabled = true;
 
+    if (chargeAmount == 1500) {
+        vendorPlan = "basic";
+    } else if (chargeAmount == 5500) {
+        vendorPlan = "business";
+    } else if (chargeAmount == 13500) {
+        vendorPlan = "premium";
+    }
+
     // Construct the request body
     const requestBody = {
-        Latitude: parseFloat(latitude.toFixed(2)),
-        Longitude: parseFloat(longitude.toFixed(2)),
-        ShippingDetails: shippingData,
-        TransCode: String(TransactionCode),
-        Orders: newOrders,
+        plan_type: vendorPlan,
+        auto_renew: isChecked,
     };
 
     try {
         // Send POST request using Fetch API and wait for the response
-        const response = await fetch('https://api.payuee.com/place-order', {
+        const response = await fetch('https://api.payuee.com/manage-subscription', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -310,14 +239,14 @@ async function placeOrder() {
             }
         }
 
-        const checkoutButton = document.getElementById('placeOrderButton');
+        const checkoutButton = document.getElementById('paymentButton');
     
         checkoutButton.disabled = false;
 
         // Return the response data so the calling function can use it
         return data;
     } catch (error) {
-        const checkoutButton = document.getElementById('placeOrderButton');
+        const checkoutButton = document.getElementById('paymentButton');
     
         checkoutButton.disabled = false;
         // Handle any errors that occur
