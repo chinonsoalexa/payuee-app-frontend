231e9ad088062c0722b5533e8e560a5adc87ac89
Chinonso <chinonsoalexander2@gmail.com>
2024-11-02 11:04:26 +0100

    working on the pricing on payuee e-Shop

---
+++ e-shop/Demo3/auth/search.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("searchField");
+    searchInput.addEventListener("input", function () {
+        const query = this.value.trim();
+
+        if (query.length > 1) { // Start searching after 1 characters
+            getSearchResults(query);
+        }
+        
+    });
+})
+
+
+function renderSearch(results) {
+    const resultsContainer = document.getElementById("searchResults");
+    resultsContainer.innerHTML = ''; // Clear previous results
+
+    if (data.length > 0) {
+        results.forEach(item => {
+        const resultItem = document.createElement("div");
+        resultItem.classList.add("col");
+
+        resultItem.innerHTML = `
+          <a href="${item.link}" class="search-result__item">
+            <img src="${item.image}" alt="${item.name}" class="search-result__image w-100">
+            <p class="search-result__name">${item.name}</p>
+          </a>
+        `;
+
+        resultsContainer.appendChild(resultItem);
+      });
+    } else {
+      resultsContainer.innerHTML = `<p>No results found</p>`;
+    }
+}
+
+async function getSearchResults(query) {
+    // Endpoint URL
+    const apiUrl = "https://api.payuee.com/search-products/" + query;
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',  // Include cookies with the request
+    };
+    
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+        
+        if (!response.ok) {
+            const data = await response.json();
+            // showToastMessageE(`response: ${data}`);
+            return;
+        }else {
+            // Process the response data
+            const data = await response.json();
+            renderSearch(data.success);
+        }
+
+    } catch (error) {
+        console.error('Error fetching user balance: ', error);
+    }
+}
