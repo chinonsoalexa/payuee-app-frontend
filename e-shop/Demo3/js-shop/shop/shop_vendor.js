b4c4a4562a4412af39dc0926eb3c4ae24d30da36
Chinonso <chinonsoalexander2@gmail.com>
2024-11-11 03:35:40 +0100

    added product recommendation for dorng

--- e-shop/Demo3/js-shop/shop/shop_vendor.js
+++ e-shop/Demo3/js-shop/shop/shop_vendor.js
@@ -327,7 +327,7 @@ function renderProducts(product) {
             <a href="https://payuee.com/e-shop/vendor/edit-product-details?ProductID=${product.ID}" class="pc__btn-wl-wrapper">
                 <button onclick="window.location.href=this.parentElement.href" class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist" title="Repost Item">
                     <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <use href="#icon_view" />
+                        <use href="#edit-content" />
                     </svg>
                 </button>
             </a>
