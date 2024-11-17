28ee70d460e898a08abfb79c34a796a921933fd6
Chinonso <chinonsoalexander2@gmail.com>
2024-11-17 20:16:58 +0100

    updating customers order history page @ payuee

---
+++ e-shop/Demo3/js-shop/profile/account_dashboard.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', async function () {
+    await getUserInfo();
+});
+
+async function getUserInfo() {
+    // Endpoint URL
+    const apiUrl = "https://api.payuee.com/profile_info";
+
+    const requestOptions = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: 'include',  // Include cookies with the request
+    };
+    
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+        
+        if (!response.ok) {
+            // const data = await response.json();
+            // showToastMessageE(`response: ${data}`);
+            // updateShippingPrices();
+            return;
+        }else {
+            const userName1 = document.getElementById('userName1');
+            const userName2 = document.getElementById('userName2');
+            // Process the response data
+            const data = await response.json();
+            user = data.user;
+
+            if (user.customer_email) {  // Check if address.customer_email exists
+                userName1.textContent = user;
+                userName2.textContent = user;
+            } else {
+                userName1.textContent = "NA";
+                userName1.textContent = "NA";
+            }
+    
+        }
+
+    } catch (error) {
+        console.error('Error fetching address data:', error);
+    }
+}
