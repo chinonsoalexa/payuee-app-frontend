9172bfd2ee59c656d79115253b231d3e1ba61f48
Chinonso <chinonsoalexander2@gmail.com>
2024-11-18 19:59:47 +0100

    updating customers order history page @ payuee

--- e-shop/Demo3/js-shop/shop/shop-outfits.js
+++ e-shop/Demo3/js-shop/shop/shop-outfits.js
@@ -16,6 +16,9 @@ var max_distance = 10;
 var min_weight = 1;
 var max_weight = 10;
 
+var stores;
+var products;
+
 // Initialize loader array with 8 elements (e.g., with null values)
 const loader = Array.from({ length: 16 }, (_, i) => i);
 
@@ -95,6 +98,8 @@ async function getProducts() {
             renderProducts(product);
         });
 
+        stores = responseData.stores;
+        products = responseData.success;
         renderStores(responseData.stores, responseData.success);
         
         NextPageOnLoad = responseData.pagination.NextPage;
@@ -226,6 +231,113 @@ function renderStores(data, products) {
         `;
 
         storeList.appendChild(listItem);
+    });
+}
+
+// Attach event listener to search input
+document.getElementById("searchField").addEventListener("input", async (event) => {
+    const searchQuery = event.target.value;
+    if (searchQuery.length > 1) {
+        await fetchProducts(searchQuery);
+    } else {
+    document.getElementById("productResults").innerHTML = ""; // Clear results when search query is too short
+    }
+});
+
+async function fetchProducts(searchTerm) {
+    const apiUrl = "https://api.payuee.com/vendor-product-search";
+
+    const requestOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: 'include', // set credentials to include cookies
+        body: JSON.stringify({
+            search_term: searchTerm,
+            vendor_id: +vendorId,
+        })
+    };
+
+    try {
+        const response = await fetch(apiUrl, requestOptions);
+
+        if (!response.ok) {
+            const errorData = await response.json();
+
+            if (errorData.error === 'failed to get user from request') {
+                // need to do a data of just null event 
+                // displayErrorMessage();
+            } else if (errorData.error === 'failed to get transaction history') {
+                // need to do a data of just null event 
+                
+            } else if  (errorData.error === 'No Authentication cookie found' || errorData.error === "Unauthorized attempt! JWT's not valid!" || errorData.error === "No Refresh cookie found") {
+                // let's log user out the users session has expired
+                // logUserOutIfTokenIsExpired();
+            }else {
+                // displayErrorMessage();
+            }
+
+            return;
+        }
+
+        const responseData = await response.json();
+        renderSearchResults(responseData.success);
+} finally {
+
+    }
+}
+
+// Function to render products in the list
+function renderSearchResults(products) {
+    const productResults = document.getElementById("productResults");
+    productResults.innerHTML = ""; // Clear previous results
+
+    if (products.length === 0) {
+        // Render a "No products found" message if there are no products
+        const noProductMessage = document.createElement("li");
+        noProductMessage.classList.add("no-products-message", "text-center", "text-muted");
+        noProductMessage.innerHTML = ``;
+        productResults.appendChild(noProductMessage);
+
+        renderStores(stores, success);
+        return;
+    }
+
+    products.forEach(product => {
+        let url = "";
+        if (product.category === "outfits") {
+            url = "https://payuee.com/outfits/" + product.product_url_id;
+        } else if (product.category === "jewelry") {
+            url = "https://payuee.com/jewelry/" + product.product_url_id;
+        } else if (product.category === "kids-accessories") {
+            url = "https://payuee.com/kids/" + product.product_url_id;
+        } else if (product.category === "cars-car-parts") {
+            url = "https://payuee.com/cars/" + product.product_url_id;
+        } else if (product.category === "tools") {
+            url = "https://payuee.com/tools/" + product.product_url_id;
+        } else if (product.category === "gadgets") {
+            url = "https://payuee.com/gadgets/" + product.product_url_id;
+        } else if (product.category === "others") {
+            url = "https://payuee.com/outfits/" + product.product_url_id;
+        }
+
+        const productItem = document.createElement("li");
+        productItem.classList.add("search-suggestion__item", "multi-select__item", "text-primary", "js-search-select", "js-multi-select");
+
+        productItem.innerHTML = `
+          <div class="d-flex align-items-center">
+            <a href="${url}" class="text-decoration-none text-dark d-flex align-items-center w-100">
+                <img src="https://payuee.com/image/${product.product_image[0].url}" alt="${product.title}" class="product-image me-3" width="50" height="50">
+                <div class="text-content">
+                    <span class="text-secondary">Title: ${product.title}</span><br>
+                    <span>Qty: ${product.stock_remaining}</span>
+                </div>
+            </a>
+          </div>
+        `;
+
+        productResults.appendChild(productItem);
     });
 }
 
