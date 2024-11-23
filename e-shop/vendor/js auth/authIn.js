ea04941a3417060ca171d38a8b0f6416013cf261
Chinonso <chinonsoalexander2@gmail.com>
2024-11-23 20:58:26 +0100

    updating payuee email organisation for orders @payuee

--- e-shop/vendor/js auth/authIn.js
+++ e-shop/vendor/js auth/authIn.js
@@ -69,19 +69,22 @@ async function check_auth_status() {
             // });
         }
 
-        if (responseData.total_products < 1) {
-            swal({
-                title: "Hi " + responseData.store_name + "!" + " welcome to Payuee e-Shop Let's get you started...",
-                icon: "success",
-                buttons: {
-                    confirm: true,
-                },
-            }).then(async (result) => {
-                if (result) {
-                    window.location.href = "update-shipping-fees.html";
-                }
-            });
+        if (window.location.pathname !== "/e-shop/vendor/update-shipping-fees") {
+            if (responseData.total_products < 1) {
+                swal({
+                    title: "Hi " + responseData.store_name + "!" + " welcome to Payuee e-Shop! Let's get you started...",
+                    icon: "success",
+                    buttons: {
+                        confirm: true,
+                    },
+                }).then(async (result) => {
+                    if (result) {
+                        window.location.href = "update-shipping-fees.html";
+                    }
+                });
+            }
         }
+        
         // Update the vendor name immediately if DOM is already loaded
         updateVendorName(responseData.store_name);
 
