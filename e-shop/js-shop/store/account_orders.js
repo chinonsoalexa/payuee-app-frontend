29ca4691d321b3d2476f458c61bee9f6d57d7452
Chinonso <chinonsoalexander2@gmail.com>
2024-11-27 06:37:11 +0100

    updating payuee email organisation for orders @payuee

--- e-shop/js-shop/store/account_orders.js
+++ e-shop/js-shop/store/account_orders.js
@@ -780,13 +780,6 @@ async function onScanSuccess(decodedText, decodedResult) {
   
   // Initialize the QR Code scanner, but don't start immediately
     let html5QrcodeScanner;
-//   const html5QrcodeScanner = new Html5QrcodeScanner(
-//     "reader", 
-//     {
-//       fps: 10,            // Frames per second for scanning
-//       qrbox: { width: 250, height: 250 } // Define scan area size
-//     }
-//   );
   
   function getProductId(id) {
     productCode = id;
@@ -849,23 +842,6 @@ async function onScanSuccess(decodedText, decodedResult) {
       productCode = ""; // Reset code after verification
     }
   }
-  
-//   function startProductScan(id) {
-//     productCode = id;
-//     const verificationStatus = document.getElementById('verificationStatus');
-//       const reader = document.getElementById('reader');
-//       verificationStatus.classList.add('hidden');
-//       reader.classList.remove('hidden');
-  
-//       // Start the QR scanner
-//       navigator.mediaDevices.getUserMedia({ video: true })
-//         .then((stream) => {
-//           html5QrcodeScanner.render(onScanSuccess, onScanFailure); // Make sure html5QrcodeScanner is initialized
-//         })
-//         .catch((error) => {
-//           console.error("Camera access denied or unavailable:", error);
-//         });
-//   }
 
   function startProductScan(id) {
     productCode = id;
@@ -876,8 +852,7 @@ async function onScanSuccess(decodedText, decodedResult) {
     reader.classList.remove('hidden');
 
   // Start the QR scanner
-  navigator.mediaDevices.getUserMedia({ video: true })
-
+//   navigator.mediaDevices.getUserMedia({ video: true })
 
     .then((stream) => {
         if (!html5QrcodeScanner) {
