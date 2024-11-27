5cfd9cee8ee49bfb4fe88cc2a42c9ceb9e9020db
Chinonso <chinonsoalexander2@gmail.com>
2024-11-27 05:38:45 +0100

    updating payuee email organisation for orders @payuee

--- e-shop/js-shop/store/account_orders.js
+++ e-shop/js-shop/store/account_orders.js
@@ -792,13 +792,6 @@ async function onScanSuccess(decodedText, decodedResult) {
   
   // Initialize the QR Code scanner, but don't start immediately
   let html5QrcodeScanner;
-//   const html5QrcodeScanner = new Html5QrcodeScanner(
-//     "reader", 
-//     {
-//       fps: 10,            // Frames per second for scanning
-//       qrbox: { width: 250, height: 250 } // Define scan area size
-//     }
-//   );
   
   function getProductId(id) {
     productCode = id;
@@ -866,7 +859,9 @@ async function onScanSuccess(decodedText, decodedResult) {
   
       verificationStatus.classList.add('hidden');
       reader.classList.remove('hidden');
-  
+
+    // Start the QR scanner
+    navigator.mediaDevices.getUserMedia({ video: true })
       if (!html5QrcodeScanner) {
           html5QrcodeScanner = new Html5QrcodeScanner("reader", {
               fps: 10,
