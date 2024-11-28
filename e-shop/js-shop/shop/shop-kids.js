afda5a8581de236e65daeb0d7a4dae9ff49cd3b1
Chinonso <chinonsoalexander2@gmail.com>
2024-11-28 03:51:02 +0100

    updating payuee email organisation for orders @payuee

--- e-shop/js-shop/shop/shop-kids.js
+++ e-shop/js-shop/shop/shop-kids.js
@@ -550,12 +550,16 @@ function renderProducts(product) {
     if (!isOutOfStock) {
         const addToCartButton = rowElement.querySelector('.pc__atc');
         addToCartButton.addEventListener('click', function(event) {
-            // Check if clothing or shoe size is empty and size is not selected
-            if (product.clothing_sizes !== "" || product.shoes_sizes !== "") {
-                event.preventDefault();
-                event.stopPropagation(); // Stop the event from propagating further
-                window.location.href = `https://payuee.com/kids/${product.product_url_id}`;
-                return;
+                     // Check if clothing or shoe size is empty and size is not selected
+            const swatchList = document.querySelector(".swatch-list");
+            if (swatchList) {
+                // Check if clothing or shoe size is empty and size is not selected
+                if (product.clothing_sizes !== "" || product.shoes_sizes !== "") {
+                    event.preventDefault();
+                    event.stopPropagation(); // Stop the event from propagating further
+                    window.location.href = `https://payuee.com/kids/${product.product_url_id}`;
+                    return;
+                }
             }
             addToCart(product);
             updateCartNumber();
