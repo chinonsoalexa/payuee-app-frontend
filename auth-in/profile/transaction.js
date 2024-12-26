d83be263267bb72e6f30cb5766fa356f53041231
Chinonso Alexander <chinonsoalexander2@gmail.com>
2024-12-27 00:47:43 +0100

    updating the save transfer transaction to the db

--- auth-in/profile/transaction.js
+++ auth-in/profile/transaction.js
@@ -162,6 +162,162 @@ document.addEventListener('DOMContentLoaded', async function () {
 });
 
 
+document.getElementById('refreshHistory').addEventListener('click', async function(event) {
+        event.preventDefault();
+
+        // Get the current URL
+        const currentUrl = new URL(window.location.href);
+
+        // Extract parameters using URLSearchParams
+        const params = new URLSearchParams(currentUrl.search);
+    
+        // Get individual parameter values
+        let pageNumber = params.get("page");
+        if (pageNumber == null) {
+            pageNumber = "1";
+        }
+        renderTransactionHistoryLoading();
+        const apiUrl = "https://api.payuee.com/transactions/" + pageNumber;
+    
+        const requestOptions = {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include', // set credentials to include cookies
+        };
+    
+        try {
+            const response = await fetch(apiUrl, requestOptions);
+    
+            if (!response.ok) {
+                const errorData = await response.json();
+    
+                if (errorData.error === 'failed to get user from request') {
+                    // need to do a data of just null event 
+                    // displayErrorMessage();
+                } else if (errorData.error === 'failed to get transaction history') {
+                    // need to do a data of just null event 
+                    
+                } else if  (errorData.error === 'No Authentication cookie found' || errorData.error === "Unauthorized attempt! JWT's not valid!" || errorData.error === "No Refresh cookie found") {
+                    // let's log user out the users session has expired
+                    logUserOutIfTokenIsExpired();
+                }else {
+                    // displayErrorMessage();
+                }
+    
+                return;
+            }
+    
+            const responseData = await response.json();
+    
+            // render the transaction history
+            renderTransactionHistory(responseData.success);
+            
+            NextPageOnLoad = responseData.pagination.NextPage;
+            PreviousPageOnLoad = responseData.pagination.PreviousPage;
+            CurrentPageOnLoad = responseData.pagination.CurrentPage;
+            TotalPageOnLoad = responseData.pagination.TotalPages;
+            TwoBeforePageOnLoad = responseData.pagination.TwoBefore;
+            TwoAfterPageOnLoad = responseData.pagination.TwoAfter;
+            ThreeAfterPageOnLoad = responseData.pagination.ThreeAfter;
+            AllRecordsOnPageLoad = responseData.pagination.AllRecords;
+            // console.log(responseData);
+            if (AllRecordsOnPageLoad > 6) {
+                // let's disable the next page navigation button
+                document.getElementById('paginationList').classList.remove('disabled');
+                document.getElementById('paginationList').disabled = false;
+            } 
+            
+            if (CurrentPageOnLoad > TotalPageOnLoad) {
+                document.getElementById('paginationList').classList.add('disabled');
+                document.getElementById('paginationList').disabled = true;
+            }
+    
+            if (CurrentPageOnLoad <= 1) {
+                deactivatePreviousButton();
+                deactivateBeforeButton();
+            } else if (CurrentPageOnLoad >= TotalPageOnLoad) {
+                deactivateNextButton();
+            }
+    
+            if (CurrentPageOnLoad < 4) {
+                // let's disable the next page navigation button
+                document.getElementById('constantBeforePage').classList.add('disabled');
+                document.getElementById('constantBeforePage').disabled = true;
+            }
+    
+            if (CurrentPageOnLoad < 5) {
+                // let's disable the next page navigation button
+                document.getElementById('dotBeforePage').classList.add('disabled');
+                document.getElementById('dotBeforePage').disabled = true;
+            }
+    
+            if (CurrentPageOnLoad > 2) {
+                // let's update the pagination with the next page
+                var currentPageElement = document.getElementById("twoBeforePage");
+                var currentPageAnchor = currentPageElement.querySelector("a");
+                currentPageAnchor.textContent = TwoBeforePageOnLoad;
+            } else {
+                // let's disable the next page navigation button
+                document.getElementById('twoBeforePage').classList.add('disabled');
+                document.getElementById('twoBeforePage').disabled = true;
+            }
+    
+            // let's update the pagination with the next page
+            var currentPageElement = document.getElementById("beforePage");
+            var currentPageAnchor = currentPageElement.querySelector("a");
+            currentPageAnchor.textContent = PreviousPageOnLoad;
+    
+            // let's update the pagination with the current page
+            var currentPageElement = document.getElementById("currentPage");
+            var currentPageAnchor = currentPageElement.querySelector("a");
+            currentPageAnchor.textContent = CurrentPageOnLoad;
+            deactivateCurrentButton();
+    
+            if (CurrentPageOnLoad >= TotalPageOnLoad) {
+                // let's disable the next page navigation button
+                document.getElementById('afterPage').classList.add('disabled');
+                document.getElementById('afterPage').disabled = true;
+            } else {
+                // let's update the pagination with the next page
+                var currentPageElement = document.getElementById("afterPage");
+                var currentPageAnchor = currentPageElement.querySelector("a");
+                currentPageAnchor.textContent = NextPageOnLoad;
+            }
+    
+            if (TwoAfterPageOnLoad < TotalPageOnLoad) {
+                // let's update the pagination with the next page
+                var currentPageElement = document.getElementById("twoAfterPage");
+                var currentPageAnchor = currentPageElement.querySelector("a");
+                currentPageAnchor.textContent = TwoAfterPageOnLoad;
+            } else {
+                // let's disable the next page navigation button
+                document.getElementById('twoAfterPage').classList.add('disabled');
+                document.getElementById('twoAfterPage').disabled = true;
+            }
+    
+            if (TwoAfterPageOnLoad > TotalPageOnLoad) {
+                // let's disable the next page navigation button
+                document.getElementById('constantAfterPage').classList.add('disabled');
+                document.getElementById('constantAfterPage').disabled = true;
+            } else {
+                // let's update the pagination with the next page
+                var currentPageElement = document.getElementById("constantAfterPage");
+                var currentPageAnchor = currentPageElement.querySelector("a");
+                currentPageAnchor.textContent = TotalPageOnLoad;
+            }
+    
+            if (ThreeAfterPageOnLoad > TotalPageOnLoad) {
+                // let's disable the next page navigation button
+                document.getElementById('dotAfterPage').classList.add('disabled');
+                document.getElementById('dotAfterPage').disabled = true;
+            }
+    } finally {
+    
+        }
+});
+
 function renderTransactionHistory(historyData) {
     // Initialize variables for the latest and oldest dates
     let latestDate = null;
@@ -809,12 +965,6 @@ function deactivateCurrentButton() {
     var resendButton = document.getElementById('currentPage');
     resendButton.classList.add('deactivated'); // Add a class to the button
 }
-
-// Adding an event listener to the anchor tag to refresh page on click on the refresh icon
-document.getElementById('refreshHistory').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent the default behavior of the anchor tag
-    location.reload(); // Call the function to reload the page
-});
 
 function getNextPageByClick(responseData, currentPageNumber) {
     // Get individual parameter values
