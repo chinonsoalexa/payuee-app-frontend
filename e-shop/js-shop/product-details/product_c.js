2dcdd13586f7a4fa844185481e3f673864fdb137
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-01-03 18:12:09 +0100

    updated the vendor tutorial steps for vendor sign up

--- e-shop/js-shop/product-details/product_c.js
+++ e-shop/js-shop/product-details/product_c.js
@@ -1251,42 +1251,42 @@ function renderRecommendedProduct(products) {
     rowElement.classList.add('swiper-slide', 'product-card'); 
     // rowElement.id = product.ID;
 
-    let urll = ""
-
-    if (product.category == "outfits") {
-        urll = "https://payuee.com/outfits/v/" + product.product_url_id;
-    } else if (product.category == "jewelry") {
-        urll = "https://payuee.com/jewelry/v/" + product.product_url_id;
-    } else if (product.category == "kids-accessories") {
-        urll = "https://payuee.com/kids/v/" + product.product_url_id;
-    } else if (product.category == "cars-car-parts") {
-        urll = "https://payuee.com/cars/v/" + product.product_url_id;
-    } else if (product.category == "tools") {
-        urll = "https://payuee.com/tools/v/" + product.product_url_id;
-    } else if (product.category == "gadgets") {
-        urll = "https://payuee.com/gadgets/v/" + product.product_url_id;
-    } else if (product.category == "others") {
-        urll = "https://payuee.com/outfits/v/" + product.product_url_id;
-    }
-    
     // Determine if the button should be disabled and what text to display
     const isOutOfStock = product.stock_remaining === 0;
     // const isOutOfStock = 7 === 0;
     const buttonText = isOutOfStock ? 'Out of Stock' : 'Add To Cart';
     const buttonDisabled = isOutOfStock ? 'disabled' : '';
 
+    let urll = ""
+
+    if (product.category == "outfits") {
+        urll = "https://payuee.com/outfits/" + product.product_url_id;
+    } else if (product.category == "jewelry") {
+        urll = "https://payuee.com/jewelry/" + product.product_url_id;
+    } else if (product.category == "kids-accessories") {
+        urll = "https://payuee.com/kids/" + product.product_url_id;
+    } else if (product.category == "cars-car-parts") {
+        urll = "https://payuee.com/cars/" + product.product_url_id;
+    } else if (product.category == "tools") {
+        urll = "https://payuee.com/tools/" + product.product_url_id;
+    } else if (product.category == "gadgets") {
+        urll = "https://payuee.com/gadgets/" + product.product_url_id;
+    } else if (product.category == "others") {
+        urll = "https://payuee.com/outfits/" + product.product_url_id;
+    }
+    
     rowElement.innerHTML = `
     <div class="pc__img-wrapper">
-        <a href="https://payuee.com/outfits/${product.product_url_id}">
+        <a href="${urll}">
             <img loading="lazy" 
-                src="urll && product.product_image.length > 0 ? product.product_image[0].url : '../../e-shop/images/default_img.png'}" 
+                src="https://payuee.com/image/${product.product_image && product.product_image.length > 0 ? product.product_image[0].url : '../../e-shop/images/default_img.png'}" 
                 width="330" 
                 height="400" 
                 alt="${product.title}" 
                 class="pc__img" 
                 onerror="this.onerror=null; this.src='../../e-shop/images/default_img.png';">
             <img loading="lazy" 
-                src="urll && product.product_image.length > 0 ? product.product_image[0].url : '../../e-shop/images/default_img.png'}" 
+                src="https://payuee.com/image/${product.product_image && product.product_image.length > 0 ? product.product_image[0].url : '../../e-shop/images/default_img.png'}" 
                 width="330" 
                 height="400" 
                 alt="${product.title}" 
@@ -1298,7 +1298,7 @@ function renderRecommendedProduct(products) {
 
       <div class="pc__info position-relative">
         <p class="pc__category">${product.category}</p>
-        <h6 class="pc__title"><a href="https://payuee.com/outfits/${product.product_url_id}">${product.title}</a></h6>
+        <h6 class="pc__title"><a href="${urll}">${product.title}</a></h6>
         <div class="product-card__price d-flex">
           <span class="money price">${formatNumberToNaira(product.reposted ? product.reposted_selling_price : product.selling_price)}</span>
         </div>
