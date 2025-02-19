bb68374ff8fa48ae8ccef679cad5d37a609fbbdf
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-02-19 10:38:39 +0100

    updating the faq admin upload backend for Payuee e-Shop

---
+++ e-shop/v/auth/analytics_out.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', async function() {
+    // Get the current URL
+    const currentUrl = new URL(window.location.href);
+
+    // Extract parameters using URLSearchParams
+    const params = new URLSearchParams(currentUrl.search);
+
+    // Define valid social media platforms
+    const validMedia = ["tiktok", "facebook", "twitter-x", "google", "instagram", "whatsapp"];
+
+    // Get individual parameter values
+    let mediaLocation = params.get("social");
+
+    // Validate mediaLocation
+    if (!validMedia.includes(mediaLocation)) {
+        mediaLocation = "others";
+    }
+
+    await getProducts();
+
+    var analyticsData = sessionStorage.getItem('site_visits_from_socials');
+
+    if (analyticsData === null) {
+        // Key does not exist in sessionStorage
+        sessionStorage.setItem('site_visits_from_socials', 'true');
+
+        fetch(`https://api.payuee.com/site-social-analytics/${mediaLocation}`, {
+            method: 'GET'
+        });
+    }
+});
