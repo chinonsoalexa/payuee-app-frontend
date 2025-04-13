28b166980ef5b094f6eeb7a0329630380f21f86b
Chinonso Alexander <chinonsoalexander2@gmail.com>
2025-04-13 16:27:24 +0100

    updated vibration on qr code scan faliure-01

--- e-shop/js-shop/shop/shop_order_tracking.js
+++ e-shop/js-shop/shop/shop_order_tracking.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async function () {
   // Get the URL parameters
   const urlParams = new URLSearchParams(window.location.search);
-  navigator.vibrate([200, 100, 200]);
+  navigator.vibrate(300); // Vibrate for 300ms
 
   // Retrieve the value of 'OrderID'
   const orderId = urlParams.get('OrderID');
@@ -53,7 +53,7 @@ async function onScanSuccess(decodedText, decodedResult) {
 // Function called when there's a scanning error (e.g., QR code not found)
 function onScanFailure(error) {
   // console.warn(`QR Code scan error: ${error}`);
-  navigator.vibrate([200, 100, 200]);
+  navigator.vibrate(300); // Vibrate for 300ms
 }
 
 // Initialize the QR Code scanner, but don't start immediately
@@ -85,7 +85,7 @@ document.getElementById("startScan").addEventListener("click", () => {
       document.getElementById('orderTrackingDetails').classList.add('hiddenn');
       document.getElementById('getOrderTrackingDetails').classList.remove('hiddenn');
       console.error("Camera access denied or unavailable:", error);
-      navigator.vibrate([200, 100, 200]);
+      navigator.vibrate(300); // Vibrate for 300ms
     });
 });
 
@@ -136,7 +136,7 @@ async function updateOrderInfo(orderId) {
         errorMessage.textContent = "Sorry, you can only track orders associated with your order history.";
         document.getElementById('orderTrackingDetails').classList.add('hiddenn');
         document.getElementById('getOrderTrackingDetails').classList.remove('hiddenn');
-        navigator.vibrate([200, 100, 200]);
+        navigator.vibrate(300); // Vibrate for 300ms
         return; // Stop further execution if there's an error
       } else if (data.error === "failed to get order history") {
           const errorMessage = document.getElementById('errorMessage');
@@ -144,7 +144,7 @@ async function updateOrderInfo(orderId) {
           errorMessage.textContent = "Wrong or invalid order detail";
           document.getElementById('orderTrackingDetails').classList.add('hiddenn');
           document.getElementById('getOrderTrackingDetails').classList.remove('hiddenn');
-          navigator.vibrate([200, 100, 200]);
+          navigator.vibrate(300); // Vibrate for 300ms
           return; // Stop further execution if there's an error
       }
       throw new Error('Failed to fetch order data.');
--- e-shop/js-shop/store/account_orders.js
+++ e-shop/js-shop/store/account_orders.js
@@ -800,7 +800,7 @@ async function onScanSuccess(decodedText, decodedResult) {
   // Function called when there's a scanning error (e.g., QR code not found)
   function onScanFailure(error) {
     // console.warn(`QR Code scan error: ${error}`);
-    navigator.vibrate([200, 100, 200]);
+    navigator.vibrate(300); // Vibrate for 300ms
   }
   
   // Initialize the QR Code scanner, but don't start immediately
@@ -852,7 +852,7 @@ async function onScanSuccess(decodedText, decodedResult) {
         verificationStatus.classList.remove('hidden');
         verificationStatus.style.color = 'red';
         verificationStatus.textContent = errorData.error || "An unknown error occurred";
-        navigator.vibrate([200, 100, 200]);
+        navigator.vibrate(300); // Vibrate for 300ms
         if (errorData.error === 'failed to get user from request' || errorData.error === 'failed to get transaction history') {
           // handle specific error cases if needed
         } else if (["No Authentication cookie found", "Unauthorized attempt! JWT's not valid!", "No Refresh cookie found"].includes(errorData.error)) {
@@ -890,7 +890,7 @@ async function onScanSuccess(decodedText, decodedResult) {
         })
         .catch((error) => {
             showToast("Camera access denied or unavailable");
-            navigator.vibrate([200, 100, 200]);
+            navigator.vibrate(300); // Vibrate for 300ms
             // console.error("Camera access denied or unavailable:", error);
         });
   }
